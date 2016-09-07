d2f426cb7d7977664d44e0fe7aca5d77a1112a36
Chase <chasefreeman13@gmail.com>
2016-09-07 18:11:29 -0400

    styled up and scroll working

--- script.js
+++ script.js
@@ -22,12 +22,22 @@ $(document).ready(function() {
 // }
   $(window).scroll(function() {
       $(".parallax-holder").css({
-      'opacity' : .85-(($(this).scrollTop())/1000)
+      'opacity' : 1 -(($(this).scrollTop())/1200)
       });
   });
   $(window).scroll(function() {
       $(".parallax-holder2").css({
-      'opacity' : 1.35 -(($(this).scrollTop())/1500)
+      'opacity' : 1.5 -(($(this).scrollTop())/1500)
       });
+  });
+  $(".foodme-scroll").click(function() {
+      $('html,body').animate({
+          scrollTop: $(".foodme-start-scroll").offset().top},
+          'slow');
+  });
+  $(".picstory-scroll").click(function() {
+      $('html,body').animate({
+          scrollTop: $(".picstory-start-scroll").offset().top},
+          'slow');
   });
 });
